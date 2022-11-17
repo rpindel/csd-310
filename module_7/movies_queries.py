c75d6c0009beef298b8c8e4847a1864858f4ebd4
Robin Pindel <robin.pindel@gmail.com>
2022-11-17 12:06:59 -0600

    Updating Moduel 7.2 assignment in repo after testing

--- module_7/movies_queries.py
+++ module_7/movies_queries.py
@@ -15,33 +15,33 @@ db = mysql.connector.connect(**config)
 cursor = db.cursor()
 cursor.execute("SELECT * FROM studio")
 studios = cursor.fetchall()
+print("\n-- DISPLAYING Studio RECORDS --")
 for studio in studios:
-print("-- DISPLAYING Studio RECORDS --")
-print("Studio ID: {}\n Studio Name: {}\n".format(studio[0], studio[1]))
+    print("Studio ID: {}\nStudio Name: {}\n".format(studio[0], studio[1]))
 
 ##query 2    
 cursor = db.cursor()
-cursor.execute("SELECT * FROM genre”)
+cursor.execute("SELECT * FROM genre")
 genres = cursor.fetchall()
+print("\n-- DISPLAYING Genre RECORDS --")
 for genre in genres:
-print("-- DISPLAYING Genre RECORDS --")
-print("Genre ID: {}\n Genre Name: {}\n".format(genre[0], genre[1]))
+    print("Genre ID: {}\nGenre Name: {}\n".format(genre[0], genre[1]))
 
 ##query 3    
 cursor = db.cursor()
-cursor.execute("SELECT film_name, runtime FROM film WHERE run_time < '120'")
+cursor.execute("SELECT film_name, film_runtime FROM film WHERE film_runtime < '120'")
 shortFilms = cursor.fetchall()
+print("\n-- DISPLAYING Short Film RECORDS --")
 for shortFilm in shortFilms:
-print("-- DISPLAYING Short Film RECORDS --")
-print("Film Name: {}\n Runtime: {}\n".format(shortFilm[0], shortFilm[1]))
+    print("Film Name: {}\nRuntime: {}\n".format(shortFilm[0], shortFilm[1]))
 
 ##query 4    
 cursor = db.cursor()
-cursor.execute("SELECT film_name, director_name,FROM film GROUP BY director_name”)
+cursor.execute("SELECT film_name, film_director FROM film ORDER BY film_director")
 directors = cursor.fetchall()
+print("\n-- DISPLAYING Director RECORDS in Order --")
 for director in directors:
-print("-- DISPLAYING Director RECORDS in Order --")
-print("Film Name: {}\n Director Name: {}\n".format(director[0], director [0]))
+    print("Film Name: {}\nDirector Name: {}\n".format(director[0], director [0]))
 
 db.close()
     
