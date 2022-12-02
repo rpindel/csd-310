029292bdb61d8bb3a350fae922ffeaf42a4ac51c
Pindel,Robin <Robin.Pindel@kohls.com>
2022-12-01 19:18:55 -0600

    Pushing code changes

--- module_10/robin_backup_create_tables.py
+++ module_10/robin_backup_create_tables.py
@@ -18,7 +18,6 @@ db = mysql.connector.connect(**config)
 cursor = db.cursor()
 
 ## drop statements
-##cursor.execute("DROP DATABASE IF EXISTS 'bacchus_wine';")
 ##cursor.execute("DROP USER IF EXISTS 'bacchus_user'@'localhost';")
 cursor.execute("DROP TABLE IF EXISTS supplier;")
 cursor.execute("DROP TABLE IF EXISTS supplies;")
@@ -43,8 +42,10 @@ cursor.execute("CREATE USER 'bacchus_user'@'localhost' IDENTIFIED WITH mysql_nat
 ##grant all privileges to the bacchus_wine database to bacchus_user on localhost
 cursor.execute("GRANT ALL PRIVILEGES ON bacchus_wine.* TO 'bacchus_user'@'localhost'")
 
+cursor.execute("USE bacchus_wine;")
+
 ##create tables
-cursor.execute("CREATE TABLE supplier (Supplier_ID INT(11) NOT NULL, Name VARCHAR(45) NOT NULL, Street_Address_1 VARCHAR(35) NOT NULL, Street_Address_2 VARCHAR(35), Zip INT(5) NOT NULL, Contact_First_Name VARCHAR(25) NOT NULL, Contact_Last_Name VARCHAR(25) NOT NULL, Phone_Number VARCHAR(15) NOT NULL, Email_Address VARCHAR(45), Order_Method ENUM('Phone', 'Post', 'Online'), Order_Method_Details VARCHAR(45), Contract_Delivery_ETA BIT(1), Contract_Delivery_ETA_Details VARCHAR(45), Active BIT(1) NOT NULL, PRIMARY KEY(Supplier_ID));")
+cursor.execute("CREATE TABLE supplier (Supplier_ID INT(11) NOT NULL, Name VARCHAR(45) NOT NULL, Street_Address_1 VARCHAR(35) NOT NULL, Street_Address_2 VARCHAR(35), Zip INT(5) NOT NULL, Contact_First_Name VARCHAR(25) NOT NULL, Contact_Last_Name VARCHAR(25) NOT NULL, Phone_Number VARCHAR(15) NOT NULL, Email_Address VARCHAR(45), Order_Method ENUM('Phone', 'Post', 'Online'), Order_Method_Details VARCHAR(45), Active BIT(1) NOT NULL, PRIMARY KEY(Supplier_ID));")
 
 ##
 cursor.execute("CREATE TABLE supplies (Supply_ID INT(11) NOT NULL, Name VARCHAR(25) NOT NULL, Description VARCHAR(45), Onhand_Quantity INT(11) NOT NULL, Unit_Price DECIMAL(6,2) NOT NULL, Supplier_ID INT(11) NOT NULL, Supply_Order_ID INT(20) NOT NULL, PRIMARY KEY(Supply_ID), CONSTRAINT fk_supplier FOREIGN KEY(Supplier_ID) REFERENCES supplier(Supplier_ID), CONSTRAINT fk_supply_order FOREIGN KEY(Supply_Order_ID) REFERENCES supply_order(Supply_Order_ID));")
